a6e76e9c3014acfdc71f38ae8a4c0b01ff3fbf05
dervvilang <147318490+dervvilang@users.noreply.github.com>
2024-11-08 23:13:50 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,19 +2,52 @@ import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
+import java.io.FileWriter;
 
 public class Main {
     public static void main(String[] args) {
 
         try (Scanner sc = new Scanner(System.in)) {
-            System.out.println("Enter the name of the input file: ");
-            String inpFileName = sc.nextLine();
+            System.out.print("Would you like to open your file or create a new one? (o - open, n - create a new one): ");
+            String fileChoice = sc.nextLine();
 
-            File inpFile = new File(inpFileName);
-            if (!inpFile.exists()) {
-                throw new IOException("File does not exist");
+            String inpFileName;
+            if (fileChoice.equalsIgnoreCase("n")) {
+                // Создание нового файла
+                System.out.print("Enter the name of the new input file: ");
+                inpFileName = sc.nextLine();
+
+                File inputFile = new File(inpFileName);
+
+                if (!inputFile.exists()) {
+                    if (inputFile.createNewFile()) {
+                        System.out.println("A new input file has been created: " + inpFileName);
+
+                        // Запрашиваем ввод текста для записи в новый файл
+                        System.out.println("Enter the text to write to the new input file. To complete, enter an empty string.");
+                        try (FileWriter writer = new FileWriter(inputFile)) {
+                            String line;
+                            while (!(line = sc.nextLine()).isEmpty()) {
+                                writer.write(line + System.lineSeparator());
+                            }
+                        }
+                        System.out.println("The input file is fulling up.");
+                    } else {
+                        throw new IOException("The input file could not be created.");
+                    }
+                } else {
+                    System.out.println("A file with that name already exists. Use it for analysis.");
+                }
             }
+            else {
+                System.out.println("Enter the name of the existing input file: ");
+                inpFileName = sc.nextLine();
 
+                File inpFile = new File(inpFileName);
+                if (!inpFile.exists()) {
+                    throw new IOException("File does not exist");
+                }
+            }
             System.out.println("Enter the name of the output file: ");
             String outFileName = sc.nextLine();
 
@@ -56,6 +89,8 @@ public class Main {
 
         } catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
+        } catch (Exception a) {
+            System.out.println("Unexpected error: " + a.getMessage());
         }
     }
 }
